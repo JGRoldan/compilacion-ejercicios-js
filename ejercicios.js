e4fece5987e2d24df8f76d0f6f06673bfa252051
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-05-24 14:15:00 -0300

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -26,3 +26,29 @@ function countPositivesSumNegatives(input) {
 const stringToNumber = function(str){
     return +str
 }
+
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+    arr=str.toLowerCase().split('')
+    contX=0
+    contO=0
+    
+    for (let i of arr){
+      console.log(arr)
+      if (i==='x') contX++
+      if (i==='o') contO++
+    }
+    
+    return contX===contO
+
+}
