7980b0d012950331ba0804ace884e10e90c74fd5
Gaston Roldan <71336562+JGRoldan@users.noreply.github.com>
2022-06-30 14:00:28 -0300

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -74,3 +74,35 @@ function passwordRandom(input){
 }
 
 console.log(passwordRandom('yourpasswordwillberandom'))
+
+// Write a function that takes in a string of one or more words, and returns the same string,
+// but with all five or more letter words reversed (Just like the name of this Kata). 
+// Strings passed in will consist of only letters and spaces. 
+// Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+const spinWords=(string)=>{
+    let arr=string.split(' ')
+    let textoFinal=[]
+    for(const idx in arr){
+      arr[idx].length>=5 
+        ?textoFinal.push(arr[idx].split('').reverse().join(''))
+        :textoFinal.push(arr[idx])
+      }
+    return textoFinal.join(' ')
+}
+
+// OR
+
+const spinWords=(string)=>{
+    return string.split(' ').map(word=>{
+    return word.length>4 
+    ? word.split('').reverse().join('')
+    : word
+    }).join(' ')
+}
+
+
